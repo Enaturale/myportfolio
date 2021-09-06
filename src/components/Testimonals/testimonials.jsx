6215cac0a2ad00b5ddf60a3cc255f49e24c0af24
The Real Tech Lady <bukolaarowosebi@gmail.com>
2021-09-07 00:56:54 +0100

    Installed SCSS

---
+++ src/components/Testimonals/testimonials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./testimonials.scss"
+
+const testimonials = () => {
+    return (
+        <div className="testimonial">
+            
+        </div>
+    )
+}
+
+export default testimonials
