2b72ecee0e36d905b4c582207cbca0dfe25ba5d2
The Real Tech Lady <bukolaarowosebi@gmail.com>
2021-09-08 10:29:15 +0100

    Scss installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import topbar from "./components/Topbar/topbar";
 
 
 function App() {
   return (
     <div className="background">
-      Hello
+      <topbar />
     </div>
   );
 }
