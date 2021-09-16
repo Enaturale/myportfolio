9975fb0fae3ca9ed57092629d055f13f7d5eb2f2
The Real Tech Lady <bukolaarowosebi@gmail.com>
2021-09-16 17:50:51 +0100

    Intro page

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -4,7 +4,18 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-            
+            <div className="left">
+                <div className="imgcontainer">
+                    <img src ="assets/image4.png" alt="Lady Bee" />                  
+                </div>
+            </div>
+
+            <div className="right">
+                <h2>Hi There, I am</h2>
+                <h1>Arowosebi Bukola</h1>
+                <h2>A Software Developer and Digital Marketer</h2>
+            </div>
+                      
         </div>
     )
 }
