bb30bbf45cd438be68ae329203c7d8f8aac1b41c
The Real Tech Lady <bukolaarowosebi@gmail.com>
2021-09-23 17:44:44 +0100

    Ityped added

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import "./intro.scss"
+import { init } from 'ityped'
+import { useEffect, useRef } from 'react'
 
 export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        console.log(textRef);
+        init(textRef.current, 
+            { showCursor: false,
+                backDelay: 2000, 
+            strings: ['Developer', 'Programming Instructor' ]
+         })
+    }, [])
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -14,8 +27,8 @@ export default function Intro() {
                 <div className ="wrapper">
                 <h2>Hi there, I am</h2>
                 <h1>Arowosebi Bukola</h1>
-                <h3>Software <span>  Developer </span> </h3>
-                <h3><span>Programming</span> Instructor</h3>
+                <h3>Software <span ref={textRef}></span> </h3>
+                {/* <h3><span>Programming</span> Instructor</h3> */}
                 </div>
 
                 <a href="#portfolio">
