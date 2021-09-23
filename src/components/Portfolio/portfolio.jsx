b9fd813238d5eff8eb1d4c05084cf4343b28f213
The Real Tech Lady <bukolaarowosebi@gmail.com>
2021-09-23 18:26:48 +0100

    Started working on portfolio section

--- src/components/Portfolio/portfolio.jsx
+++ src/components/Portfolio/portfolio.jsx
@@ -4,6 +4,20 @@ import "./portfolio.scss"
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
+            <h1>My Portfolio</h1>
+            <ul>
+                <li className="active">Featured </li>
+                <li>Web Apps </li>
+                <li>Mobile Apps</li>
+            </ul>
+
+            <div className="container">
+                <div className="item">
+                    <img src="" alt="E-commerce app"/>
+                    <h3>E-Commerce App</h3>
+
+                </div>
+            </div>
             
         </div>
     )
