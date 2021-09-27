c04204ef11387ac681b3b32fa29df12d34066df1
The Real Tech Lady <bukolaarowosebi@gmail.com>
2021-09-27 17:21:31 +0100

    working on slider

--- src/components/Works/works.jsx
+++ src/components/Works/works.jsx
@@ -1,28 +1,56 @@
 import React from 'react'
 import "./works.scss"
 
+
+
 export default function Works() {
+    const data = [
+        {
+            id: "1",
+            icon: "assets/marketing.jpg",
+            title: "Marketing",
+            desc: "5 Free and Top Digital Marketing Tools for Effective Sales and Fast, Rapid Business Growth.",
+            img: "assets/growthhacking.jpeg",
+        },
+        {
+            id: "2",
+            icon: "assets/webapp.png",
+            title: "Web Application",
+            desc: "AN E-Commerce Website using Reacj js and Stripe for payment",
+            img: "assets/ecommerce.png",
+        },   
+        {
+            id: "3",
+            icon: "assets/mobile.png",
+            title: "Mobile Application",
+            desc: "Food Delivery Application",
+            img: "assets/chatapp.jpg",
+        },
+    ]
     return (
         <div className="works" id="works">
-            <div className="slider">
+            <div className="slider" >
+                {data.map(d => (
                 <div className="container">
                     <div className="item">
                         <div className="left">
                             <div className="leftContainer">
                                 <div className="imgContainer">
-                                    <img src ="assets/moicon.png" alt=""/>
+                                    <img src ={d.icon} alt=""/>
                                 </div>
-                                <h2>Title</h2>
-                                <p>What the fck is this supposed to be?</p>
+                                <h2>{d.title}</h2>
+                                <p>{d.desc}</p>
                                 <span>Projects </span>
-                            </div> 
+                            </div>  
                         </div>
 
                         <div className="right">
-                            <img src="assets/voicetech.png" alt="Voice tech work"  />
+                            <img src={d.img} alt="Voice tech work"  />
                         </div>
                     </div>
+                    
                 </div>
+                ))}
             </div>
 
             <img src="assets/arrowleft.png" className="arrow Left" alt="arrow left"/>
