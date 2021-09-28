c53de0267371a257788f58fd8621e15a2bd33af6
The Real Tech Lady <bukolaarowosebi@gmail.com>
2021-09-28 13:32:35 +0100

    contact section started

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import {useState} from 'react';
 import "./contact.scss";
 
 export default function Contact() {
+    const[message, setMessage] = useState(false);
+
+    const handleSubmit =(e)=>{
+        e.preventDefault();
+        setMessage(true);
+    }
     return (
         <div className="contact" id="contact"> 
+            <div className="left">
+                <img src="assets/contactme.png" alt="Contact" />
+            </div>
+            
+            <div className="right">
+                <h2>Contact.</h2>
+                <form  onSubmit={handleSubmit}> 
+                    <input type="text" placeholder="Email"></input>
+                     <textarea placeholder="Message"></textarea>
+                     <button type="submit">Send</button>
+                </form>
+            </div>
             
         </div>
     )
